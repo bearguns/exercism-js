fb19d6458843ed83b125fa6864af1f1a38c7cbea
Sean <sean@layer3tv.com>
2018-01-03 09:40:49 -0700

    full solution

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,12 +1,7 @@
-var TwoFer = function () {};
-
-TwoFer.prototype.twoFer = function (who) {
-  // your code goes here
-  // You will have to use the parameter who
-  // in some way. In this example, it is just
-  // returned, but your solution will have to
-  // use a conditional.
-  return 'One for ' + (who || 'you') + ', one for me.';
-};
+class TwoFer {
+  twoFer(name) {
+    return `One for ${name || 'you'}, one for me.`;
+  }
+}
 
 module.exports = TwoFer;
